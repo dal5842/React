4e79fc4cfbd23c53ef34eaafac152bf1f9840c60
Dannika <dal5842@psu.edu>
2025-12-16 19:43:10 -0500

    main jsx update

--- reactApp/src/main.jsx
+++ reactApp/src/main.jsx
@@ -1,17 +1,10 @@
-// src/main.jsx
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";  // only if you still use global CSS, otherwise omit
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-    <React.StrictMode>
-        <HashRouter basename="/">
-            <App />
-        </HashRouter>
-    </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+)
